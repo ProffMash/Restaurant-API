61b10e8bef9533f7ae03c92dd5632d553716b194
David <kdavidmacharia8485@gmail.com>
2024-06-13 16:49:41 +0300

    bear auth

--- src/middleware/bearAuth.ts
+++ src/middleware/bearAuth.ts
@@ -34,3 +34,4 @@ export const restaurant_ownerRoleAuth = async (c: Context, next: Next) => await 
 export const bothadmindriver = async(c:Context , next: Next)=> await authMiddleware(c, next, "driver" || "admin")
 export const bothuserres = async(c:Context , next: Next)=> await authMiddleware(c, next, "user" || "restaurant_owner")
 export const bothdriveruseradmin =async(c:Context, next:Next)=> await authMiddleware(c, next, "user" || "admin" || "restaurant_owner")
+export const bothresdriver =async(c:Context, next:Next)=> await authMiddleware(c, next, "driver" || "restaurant_owner")
